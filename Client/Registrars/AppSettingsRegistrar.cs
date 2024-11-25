56eb7e458e296923adef5a29bd7b8559d59f9277
dajuan323 <83599259+dajuan323@users.noreply.github.com>
2024-11-25 18:26:19 -0500

    Updating request address for Client to Server Login
    Signed-off-by: dajuan323 <83599259+dajuan323@users.noreply.github.com>

--- Client/Registrars/AppSettingsRegistrar.cs
+++ Client/Registrars/AppSettingsRegistrar.cs
@@ -8,12 +8,14 @@ public class AppSettingsRegistrar : IWebAssemblyHostBuilderRegistrar
 {
     public void RegisterServices(WebAssemblyHostBuilder builder)
     {
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("blazor-emp-mgmt-backend.azurewebsites.net") });
+        
         builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
         builder.Services.AddScoped<AllState>();
 
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
-         
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("blazor-emp-mgmt-backend.azurewebsites.net") });
+
     }
 }
