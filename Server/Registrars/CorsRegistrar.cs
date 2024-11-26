5826ff4e5a06b3f8afa8181581b10addb0839864
dajuan323 <83599259+dajuan323@users.noreply.github.com>
2024-11-25 19:00:17 -0500

    Updated CORS Registrar in Server
    Signed-off-by: dajuan323 <83599259+dajuan323@users.noreply.github.com>

--- Server/Registrars/CorsRegistrar.cs
+++ Server/Registrars/CorsRegistrar.cs
@@ -12,7 +12,6 @@ public class CorsRegistrar : IWebApplicationBuilderRegistrar
                 .WithOrigins(
                     "http://localhost:5104",
                     "https://localhost:7116", 
-                    "https://blzr-emp-mgmt-backend.azurewebsites.net",
                     "https://salmon-moss-0c377bb0f.5.azurestaticapps.net")
                 .AllowAnyMethod()
                 .AllowAnyHeader()
